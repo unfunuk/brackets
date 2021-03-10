678557265c0caa3556130bce4b90cd57d973118e
unfunuk <a.s.s.t.e.r.i.x.0@gmail.com>
2021-03-10 16:54:22 +0300

    feat: fix

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let brackets = str;
+    let halfLength = Math.floor(brackets.length / 2);
+    for (let i = 0; i < halfLength; i++) {
+        for (let j = 0; j < bracketsConfig.length; j++) {
+
+            brackets = brackets.replace(bracketsConfig[j].join(''), '');
+        }
+    }
+    return brackets.length === 0;
 }
